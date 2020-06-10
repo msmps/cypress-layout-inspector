9c46be1eb92f4cb3584cf518080e55d5d22bace9
Matthew Simpson <64437301+msmps@users.noreply.github.com>
2020-06-10 17:38:02 +0100

    feat(position): added above and below assertions to position category

--- cypress/integration/position.spec.js
+++ cypress/integration/position.spec.js
@@ -3,11 +3,27 @@ describe('position testing commands', () => {
         cy.visit('cypress/fixtures/test-app/');
     });
 
-    it('block-2 should be positioned right of block-1', () => {
-        cy.get('.block-2').should('be.rightOf', '.block-1', 50);
+    it('block-2 should be positioned right of block-1 (and by 50px)', () => {
+        cy.get('.block-2')
+            .should('be.rightOf', '.block-1')
+            .should('be.rightOf', '.block-1', 50);
     });
 
-    it('block-3 should be positioned left of block-4', () => {
-        cy.get('.block-3').should('be.leftOf', '.block-4', 50);
+    it('block-3 should be positioned left of block-4 (and by 50px)', () => {
+        cy.get('.block-3')
+            .should('be.leftOf', '.block-4')
+            .should('be.leftOf', '.block-4', 50);
+    });
+
+    it('block-5 should be positioned above block-6 (and by 50px)', () => {
+        cy.get('.block-5')
+            .should('be.above', '.block-6')
+            .should('be.above', '.block-6', 50);
+    });
+
+    it('block-8 should be positioned below block-7 (and by 50px)', () => {
+        cy.get('.block-8')
+            .should('be.below', '.block-7')
+            .should('be.below', '.block-7', 50);
     });
 });
--- src/assertions/position.js
+++ src/assertions/position.js
@@ -5,6 +5,14 @@ export default _chai => {
         const [source, target] = [new Rect(this._obj), new Rect(element)];
         const actual = source.left - target.right;
 
+        if (distance === 0) {
+            return this.assert(
+                actual >= 0,
+                `expected #{this} to be right of ${element}`,
+                `expected #{this} not to be right of ${element}`
+            );
+        }
+
         return this.assert(
             actual === distance,
             `expected #{this} to be right of ${element} by #{exp}, but the value was #{act}`,
@@ -18,6 +26,14 @@ export default _chai => {
         const [source, target] = [new Rect(this._obj), new Rect(element)];
         const actual = target.left - source.right;
 
+        if (distance === 0) {
+            return this.assert(
+                actual >= 0,
+                `expected #{this} to be left of ${element}`,
+                `expected #{this} not to be left of ${element}`
+            );
+        }
+
         return this.assert(
             actual === distance,
             `expected #{this} to be left of ${element} by #{exp}, but the value was #{act}`,
@@ -29,4 +45,64 @@ export default _chai => {
 
     _chai.Assertion.addMethod('rightOf', rightOf);
     _chai.Assertion.addMethod('leftOf', leftOf);
+
+    _chai.Assertion.overwriteMethod('above', _super => {
+        return function (element, distance = 0) {
+            if (this._obj.constructor.name === 'jQuery') {
+                const [source, target] = [
+                    new Rect(this._obj),
+                    new Rect(element),
+                ];
+                const actual = target.top - source.bottom;
+
+                if (distance === 0) {
+                    return this.assert(
+                        actual >= 0,
+                        `expected #{this} to be above ${element}`,
+                        `expected #{this} not to be above ${element}`
+                    );
+                }
+
+                return this.assert(
+                    actual === distance,
+                    `expected #{this} to be above ${element} by #{exp}, but the value was #{act}`,
+                    `expected #{this} not to be above ${element} by #{exp}, but the value was #{act}`,
+                    distance,
+                    actual
+                );
+            }
+
+            return _super.apply(this, arguments);
+        };
+    });
+
+    _chai.Assertion.overwriteMethod('below', _super => {
+        return function (element, distance = 0) {
+            if (this._obj.constructor.name === 'jQuery') {
+                const [source, target] = [
+                    new Rect(this._obj),
+                    new Rect(element),
+                ];
+                const actual = source.top - target.bottom;
+
+                if (distance === 0) {
+                    return this.assert(
+                        actual >= 0,
+                        `expected #{this} to be below ${element}`,
+                        `expected #{this} not to be below ${element}`
+                    );
+                }
+
+                return this.assert(
+                    actual === distance,
+                    `expected #{this} to be below ${element} by #{exp}, but the value was #{act}`,
+                    `expected #{this} not to be below ${element} by #{exp}, but the value was #{act}`,
+                    distance,
+                    actual
+                );
+            }
+
+            return _super.apply(this, arguments);
+        };
+    });
 };
